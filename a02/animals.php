383fec73fafb5e5a7b14d517e75fb263af453b02
Tian Yang <tyang2896@conestogac.on.ca>
2024-10-12 21:24:48 -0400

    Add theZoo

---
+++ a02/animals.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$username = $_POST["username"];
+$animal = $_POST["animal"];
+
+<head> 
+<title> Hello $username </title>
+<\head>
+
+<body>
+print($username);
+if($animal == "tiger")
+{
+    <img src="theZoo/tiger.jpg" alt="">
+}
+</body>
+?>
+
+</html>
