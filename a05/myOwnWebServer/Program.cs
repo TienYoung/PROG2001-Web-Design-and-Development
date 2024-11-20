9e9698412ab7b161041d5bcd0e47b486699b7222
Tian Yang <tyang2896@conestogac.on.ca>
2024-11-19 22:55:20 -0500

    Tested index.html file sending.

--- a05/myOwnWebServer/Program.cs
+++ a05/myOwnWebServer/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -9,9 +9,9 @@ namespace myOwnWebServer
 {
     internal class Program
     {
-        static readonly string webRootPattern = @"-webRoot=([^\s]+)";
-        static readonly string webIPPattern = @"-webIP=([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)";
-        static readonly string webPortPattern = @"-webPort=([0-9]+)";
+        private static readonly string webRootPattern = @"-webRoot=([^\s]+)";
+        private static readonly string webIPPattern = @"-webIP=([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)";
+        private static readonly string webPortPattern = @"-webPort=([0-9]+)";
 
         static void Main(string[] args)
         {
@@ -29,20 +29,30 @@ namespace myOwnWebServer
                     using (NetworkStream netStream = client.GetStream())
                     {
                         byte[] buffer = new byte[client.ReceiveBufferSize];
-                        int readSize = netStream.Read(buffer, 0, client.ReceiveBufferSize);
-                        byte[] data = new byte[readSize];
-                        Array.Copy(buffer, data, readSize);
+                        //while (true)
+                        {
+                            int readSize = netStream.Read(buffer, 0, client.ReceiveBufferSize);
+                            byte[] data = buffer.Take(readSize).ToArray();
 
-                        string httpHeader = System.Text.Encoding.ASCII.GetString(data);
-                        Console.WriteLine(httpHeader);
+                            string request = System.Text.Encoding.ASCII.GetString(data);
+                            Console.WriteLine(request);
+
+                            FileStream fileStream = new FileStream("./index.html", FileMode.Open);
+
+                            string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + fileStream.Length + "\r\n\r\n";
+                            data = System.Text.Encoding.ASCII.GetBytes(response);
+                            netStream.Write(data, 0, data.Length);
+                            
+                            fileStream.CopyTo(netStream);
+                        }
                     }
                 }
                 server.Stop();
                 Console.WriteLine("Server Stopped!");
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.Error.WriteLine(ex.ToString());
+                Console.Error.WriteLine(e);
             }
         }
     }
