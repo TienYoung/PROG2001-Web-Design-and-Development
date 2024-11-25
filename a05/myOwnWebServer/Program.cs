3f245aa5ae721c0401fb3ff90f21fd0d2437e5ed
Tian Yang <tyang2896@conestogac.on.ca>
2024-11-25 18:56:16 -0500

    Update server loop.

--- a05/myOwnWebServer/Program.cs
+++ a05/myOwnWebServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -19,40 +20,72 @@ namespace myOwnWebServer
             string webIP = Regex.Match(args[1], webIPPattern).Groups[1].Value;
             string webPort = Regex.Match(args[2], webPortPattern).Groups[1].Value;
 
-            try
+            TcpListener server = new TcpListener(IPAddress.Parse(webIP), Convert.ToInt32(webPort));
+            server.Start();
+            while (true)
             {
-                TcpListener server = new TcpListener(IPAddress.Parse(webIP), Convert.ToInt32(webPort));
-                server.Start();
-                Console.WriteLine("Server Started!");
                 using (TcpClient client = server.AcceptTcpClient())
                 {
                     using (NetworkStream netStream = client.GetStream())
                     {
                         byte[] buffer = new byte[client.ReceiveBufferSize];
-                        //while (true)
+
+                        int readSize = netStream.Read(buffer, 0, client.ReceiveBufferSize);
+                        byte[] data = buffer.Take(readSize).ToArray();
+
+                        string request = System.Text.Encoding.ASCII.GetString(data);
+                        Console.WriteLine($"Request:\n{request}");
+
+                        Dictionary<string, string> headers = new Dictionary<string, string>();
+
+                        string[] lines = request.Split('\n');
+                        string[] parts = lines[0].Split(' ');
+                        if (parts.Length != 3)
                         {
-                            int readSize = netStream.Read(buffer, 0, client.ReceiveBufferSize);
-                            byte[] data = buffer.Take(readSize).ToArray();
+                            throw new Exception("Header invalid!");
+                        }
+                        string method = parts[0];
+                        string path = parts[1];
+                        string version = parts[2];
 
-                            string request = System.Text.Encoding.ASCII.GetString(data);
-                            Console.WriteLine(request);
+                        for (int i = 1; i < lines.Length; i++)
+                        {
+                            if (String.IsNullOrWhiteSpace(lines[i]))
+                            {
+                                continue;
+                            }
 
-                            FileStream fileStream = new FileStream("./index.html", FileMode.Open);
+                            parts = lines[i].Split(new[] { ": " }, 2, StringSplitOptions.None);
+                            if (parts.Length != 2)
+                            {
+                                throw new Exception("Header invalid!");
+                            }
 
-                            string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + fileStream.Length + "\r\n\r\n";
+                            headers.Add(parts[0], parts[1]);
+                        }
+
+                        string response = null;
+                        if (method == "GET")
+                        {
+                            using (FileStream fileStream = new FileStream("." + path, FileMode.Open))
+                            {
+                                response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + fileStream.Length + "\r\n\r\n";
+                                data = System.Text.Encoding.ASCII.GetBytes(response);
+                                netStream.Write(data, 0, data.Length);
+                                fileStream.CopyTo(netStream);
+                            }
+                        }
+                        else
+                        {
+                            response = "HTTP/1.1 404 Not Found\r\n";
+                            response += "Connection: Close\r\n";
+                            response += "Content-Length: 0\r\n";
+                            response += "\r\n";
                             data = System.Text.Encoding.ASCII.GetBytes(response);
                             netStream.Write(data, 0, data.Length);
-                            
-                            fileStream.CopyTo(netStream);
                         }
                     }
                 }
-                server.Stop();
-                Console.WriteLine("Server Stopped!");
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e);
             }
         }
     }
