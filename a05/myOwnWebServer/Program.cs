cc96954c6829e9c41cf0865d684a4b806e34652b
Tian Yang <tyang2896@conestogac.on.ca>
2024-11-25 19:26:02 -0500

    Added ParseRequestHeader

--- a05/myOwnWebServer/Program.cs
+++ a05/myOwnWebServer/Program.cs
@@ -36,45 +36,18 @@ namespace myOwnWebServer
                         string request = System.Text.Encoding.ASCII.GetString(data);
                         Console.WriteLine($"Request:\n{request}");
 
-                        Dictionary<string, string> requestHeader = new Dictionary<string, string>();
-
-                        string[] lines = request.Split('\n');
-                        string[] parts = lines[0].Split(' ');
-                        if (parts.Length != 3)
-                        {
-                            throw new Exception("Header invalid!");
-                        }
-                        string method = parts[0];
-                        string path = parts[1];
-                        string version = parts[2];
-
-                        for (int i = 1; i < lines.Length; i++)
-                        {
-                            if (String.IsNullOrWhiteSpace(lines[i]))
-                            {
-                                continue;
-                            }
-
-                            parts = lines[i].Split(new[] { ": " }, 2, StringSplitOptions.None);
-                            if (parts.Length != 2)
-                            {
-                                throw new Exception("Header invalid!");
-                            }
-
-                            requestHeader.Add(parts[0], parts[1]);
-                        }
-
-                        Dictionary<string, string> responseHeader = new Dictionary<string, string>();
+                        Dictionary<string, string> requestDict = ParseRequestHeader(request);
 
                         string response = null;
-                        if (method == "GET")
+                        if (requestDict["Method"] == "GET")
                         {
-                            using (FileStream fileStream = new FileStream("." + path, FileMode.Open))
+                            using (FileStream fileStream = new FileStream("." + requestDict["URL"], FileMode.Open))
                             {
-                                responseHeader["Response"] = "HTTP/1.1 200 OK";
-                                responseHeader["Content-Type"] = "text/html";
-                                responseHeader["Content-Length"] = fileStream.Length.ToString();
-                                response = GenerateResponseString(responseHeader);
+                                Dictionary<string, string> responseDict = new Dictionary<string, string>();
+                                responseDict["Response"] = "HTTP/1.1 200 OK";
+                                responseDict["Content-Type"] = "text/html";
+                                responseDict["Content-Length"] = fileStream.Length.ToString();
+                                response = GenerateResponseString(responseDict);
 
                                 data = System.Text.Encoding.ASCII.GetBytes(response);
                                 netStream.Write(data, 0, data.Length);
@@ -83,10 +56,12 @@ namespace myOwnWebServer
                         }
                         else
                         {
-                            response = "HTTP/1.1 404 Not Found\r\n";
-                            response += "Connection: Close\r\n";
-                            response += "Content-Length: 0\r\n";
-                            response += "\r\n";
+                            Dictionary<string, string> responseDict = new Dictionary<string, string>();
+                            responseDict["Response"] = "HTTP/1.1 404 Not Found";
+                            responseDict["Connection"] = "Close";
+                            responseDict["Content-Length"] = "0";
+                            response = GenerateResponseString(responseDict);
+
                             data = System.Text.Encoding.ASCII.GetBytes(response);
                             netStream.Write(data, 0, data.Length);
                         }
@@ -97,14 +72,42 @@ namespace myOwnWebServer
 
         static Dictionary<string, string> ParseRequestHeader(string request)
         {
-            return null;
+            Dictionary<string, string> headerDict = new Dictionary<string, string>();
+
+            string[] lines = request.Split('\n');
+            string[] parts = lines[0].Split(' ');
+            if (parts.Length != 3)
+            {
+                throw new Exception("Request invalid!");
+            }
+            headerDict["Method"] = parts[0];
+            headerDict["URL"] = parts[1];
+            headerDict["Version"] = parts[2];
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(lines[i]))
+                {
+                    continue;
+                }
+
+                parts = lines[i].Split(new[] { ": " }, 2, StringSplitOptions.None);
+                if (parts.Length != 2)
+                {
+                    Console.Error.WriteLine("Request message error: {0}", lines[i]);
+                    continue;
+                }
+
+                headerDict.Add(parts[0], parts[1]);
+            }
+            return headerDict;
         }
 
-        static string GenerateResponseString(Dictionary<string, string> header)
+        static string GenerateResponseString(Dictionary<string, string> headerDict)
         {
-            string response = header["Response"] + "\r\n";
-            header.Remove("Response");
-            foreach (var message in header)
+            string response = headerDict["Response"] + "\r\n";
+            headerDict.Remove("Response");
+            foreach (var message in headerDict)
             {
                 response += message.Key + ": " + message.Value + "\r\n";
             }
